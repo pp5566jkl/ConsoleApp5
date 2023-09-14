df68609c1bb2ebbf632377ddec8a2455b89e1417
CSIE <CSIE@C303-30>
2023-09-14 16:36:05 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using ConsoleApp5;
+using System.Collections.Generic;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<drink> drinks = new List<drink>();
+
+            addnewdrink(drinks);
+
+            displaydrinkmenu(drinks);
+        }
+
+        private static void displaydrinkmenu(List<drink> mydrinks)
+        {
+
+            Console.WriteLine("飲料清單");
+            Console.WriteLine("--------------------------");
+            int i = 0;
+            Console.WriteLine(String.Format("{0,-4}{1,-5}{2,-5}{3,-5}", "編號", "品名", "大小", "價格"));
+            foreach (drink drink in mydrinks)
+            {
+                Console.WriteLine($"{i,-6}{drink.Name,-5}{drink.Size,-5}{drink.Price,5:C0}");
+                i++;
+            }
+            Console.WriteLine("--------------------------");
+        }
+
+        private static void addnewdrink(List<drink> drinks)
+        {
+            drinks.Add(new drink() { Name = "紅茶", Size = "大杯", Price = 50 });
+            drinks.Add(new drink() { Name = "紅茶", Size = "小杯", Price = 30 });
+            drinks.Add(new drink() { Name = "綠茶", Size = "大杯", Price = 50 });
+            drinks.Add(new drink() { Name = "綠茶", Size = "小杯", Price = 30 });
+            drinks.Add(new drink() { Name = "咖啡", Size = "大杯", Price = 70 });
+            drinks.Add(new drink() { Name = "咖啡", Size = "小杯", Price = 50 });
+        }
+    }
+}
---
+++ drink.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class drink
+    {
+        public String Name { get; set; }
+        public String Size { get; set; }
+        public int Price { get; set; }
+    }
+}
